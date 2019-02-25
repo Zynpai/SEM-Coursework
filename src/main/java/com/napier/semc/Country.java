933b7a783beb823afb26cdc5fbbf2803cb516171
Viktor Malych <victormalych@gmail.com>
2019-02-25 13:25:43 +0000

    Added Country class

---
+++ src/main/java/com/napier/semc/Country.java
@@ -0,0 +1,38 @@
+package com.napier.semc;
+
+/**
+ * Represents a country
+ */
+
+public class Country {
+    //Country code
+    public String code;
+    //Country name
+    public String name;
+    //Country continent
+    public String continent;
+    //Country region
+    public String region;
+    //Country surface area
+    public double surfaceArea;
+    //Country year of independence
+    public int indYear;
+    //Country population
+    public int population;
+    //Country life expectancy
+    public double lifeExpectancy;
+    //Country GNP
+    public double GNP;
+    //Country old GNP
+    public double oldGNP;
+    //Country local name
+    public String localName;
+    //Country government form
+    public String govForm;
+    //Country head of state
+    public String headOfState;
+    //Country capital
+    public String capital;
+    //Country code 2
+    public String code2;
+}
