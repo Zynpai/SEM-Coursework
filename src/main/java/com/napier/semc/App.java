15eb9ceb9ce9ee31f233e6febcb1a4d7d140be02
40322938 <killeroid356@gmail.com>
2019-03-20 12:18:44 +0000

    time change

--- src/main/java/com/napier/semc/App.java
+++ src/main/java/com/napier/semc/App.java
@@ -26,14 +26,14 @@ public class App
             System.exit(-1);
         }
 
-        int retries = 15;
+        int retries = 10;
         for (int i = 0; i < retries; ++i)
         {
             System.out.println("Connecting to database...");
             try
             {
                 // Wait a bit for db to start
-                Thread.sleep(20000);
+                Thread.sleep(30000);
                 // Connect to database
                 con = DriverManager.getConnection("jdbc:mysql://" + location + "/world?allowPublicKeyRetrieval=true&useSSL=false", "root", "example");
                 System.out.println("Successfully connected");
