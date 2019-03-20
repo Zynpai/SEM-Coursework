e3400d798a1954d3292b2365a8258df640827d8f
40322938 <killeroid356@gmail.com>
2019-03-20 10:56:25 +0000

    moved tests to semc

--- src/test/java/com/napier/semc/MyTest.java
+++ src/test/java/com/napier/semc/MyTest.java
@@ -1,4 +1,4 @@
-package com.napier.sem;
+package com.napier.semc;
 
 import com.napier.semc.App;
 import org.junit.jupiter.api.*;
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.*;
 class MyTest
 {
     static App app;
-    static String Asia = "Asian";
+    static String Asia = "Asia";
 
     @BeforeAll
     static void init()
