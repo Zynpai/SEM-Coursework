0c018a6a7d20b915e495b8007d3253b890c738c1
killeroid356 <34097198+killeroid356@users.noreply.github.com>
2019-04-02 15:13:17 +0100

    added a few more integration tests

--- src/test/java/com/napier/semc/Integration_tests.java
+++ src/test/java/com/napier/semc/Integration_tests.java
@@ -93,6 +93,11 @@ public class Integration_tests {
         assertEquals(0, pop.size());
     }
     @Test
+    void testcontinentpopulations_empty(){
+        ArrayList<Country> pop = app.getContinentPopulations("");
+        assertEquals(0, pop.size());
+    }
+    @Test
     void testcontinentpopulations_real(){
         ArrayList<Country> pop = app.getContinentPopulations("Europe");
         assertNotEquals(0, pop.size());
@@ -106,10 +111,83 @@ public class Integration_tests {
         assertEquals(0,pop.size());
     }
     @Test
+    void getRegionPopulationsEmpty(){
+        ArrayList<Country> pop = app.getRegionPopulations("");
+        assertEquals(0,pop.size());
+    }
+    @Test
     void getRegionPopulationsReal(){
         ArrayList<Country> pop = app.getRegionPopulations("Eastern Europe");
         assertNotEquals(0,pop.size());
     }
 
+//getCountryPopulations
 
+    @Test
+    void getCountryPopulationsGarbage(){
+        ArrayList<Country> pop = app.getCountryPopulations("adasfewfwdsf");
+        assertEquals(0,pop.size());
+    }
+    @Test
+    void getCountryPopulationsEmpty(){
+        ArrayList<Country> pop = app.getCountryPopulations("");
+        assertEquals(0,pop.size());
+    }
+    @Test
+    void getCountryPopulationsReal(){
+        ArrayList<Country> pop = app.getCountryPopulations("Russia");
+        assertNotEquals(0,pop.size());
+    }
+
+//getDistrictPopulations
+    @Test
+    void getDistrictPopulationsGarbage(){
+        ArrayList<City> pop = app.getDistrictPopulations("adasfewfwdsf");
+        assertEquals(0,pop.size());
+    }
+    @Test
+    void getDistrictPopulationsEmpty(){
+        ArrayList<City> pop = app.getDistrictPopulations("");
+        assertEquals(0,pop.size());
+    }
+    @Test
+    void getDistrictPopulationsReal(){
+        ArrayList<City> pop = app.getDistrictPopulations("Moscow (City)");
+        assertNotEquals(0,pop.size());
+    }
+
+
+//getCityPopulations
+
+    @Test
+    void getCityPopulationsGarbage(){
+        ArrayList<City> pop = app.getCityPopulations("adasfewfwdsf");
+        assertEquals(0,pop.size());
+    }
+    @Test
+    void getCityPopulationsEmpty(){
+        ArrayList<City> pop = app.getCityPopulations("");
+        assertEquals(0,pop.size());
+    }
+    @Test
+    void getCityPopulationsReal(){
+        ArrayList<City> pop = app.getCityPopulations("Moscow");
+        assertNotEquals(0,pop.size());
+    }
+
+//getWorldTopCapitalCities
+
+    @Test
+    void getWorldTopCapitalCitiesReal(){
+        app.getWorldTopCapitalCities();
+    }
+
+
+//getWorldTopCountries
+
+    @Test
+    void getWorldTopCountriesReal(){
+        app.getWorldTopCountries();
+    }
+    
 }
